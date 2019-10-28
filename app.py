532a985dff93795d9e933ce93d9855fd26c6b2e3
Galleom <enc.eng@uea.edu.br>
2019-10-27 22:14:46 -0400

    generator add
    added base generator code

--- app.py
+++ app.py
@@ -3,8 +3,42 @@ from flask import Flask, request, jsonify
 import os
 app = Flask(__name__)
 
+import generate
+def init():
+    global graph
+    graph = generate.load_graph('.', 'text_model')
+
 @app.route('/getmsg/', methods=['GET'])
 def respond():
+    return "<h1>Welcome to our server !!</h1>"
+
+@app.route('/result', methods=['GET'])
+def get_something():
+    return jsonify({
+        "Message": generate.predict(graph[0], graph[1], graph[2],graph [3]),
+        # Add this option to distinct the POST request
+        "METHOD" : "GET"
+    })
+
+@app.route('/results/', methods=['POST'])
+def post_something():
+    param = request.form.get('name')
+    print(param)
+    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
+    if param:
+        return jsonify({
+            "Message": f"Welcome {param} to our awesome platform!!",
+            # Add this option to distinct the POST request
+            "METHOD" : "POST"
+        })
+    else:
+        return jsonify({
+            "ERROR": "no name found, please send a name."
+        })
+
+# A welcome message to test our server
+@app.route('/')
+def index():
     # Retrieve the name from url parameter
     name = request.args.get("name", None)
 
@@ -25,27 +59,6 @@ def respond():
 
     # Return the response in json format
     return jsonify(response)
-
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
-
-# A welcome message to test our server
-@app.route('/')
-def index():
-    return "<h1>Welcome to our server !!</h1>"
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
---
+++ generate.py
@@ -0,0 +1,43 @@
+import json
+import os
+import numpy as np
+import tensorflow as tf
+
+import model, sample, encoder
+
+def load_graph(models_dir, model_name, temperature=0.8, top_k=40,top_p=1,batch_size=1, length=None, seed = None):
+    models_dir = os.path.expanduser(os.path.expandvars(models_dir))
+    enc = encoder.get_encoder(model_name, models_dir)
+    hparams = model.default_hparams()
+    with open(os.path.join(models_dir, model_name, 'hparams.json')) as f:
+        hparams.override_from_dict(json.load(f))
+
+    if length is None:
+        length = hparams.n_ctx
+    elif length > hparams.n_ctx:
+        raise ValueError("Can't get samples longer than window size: %s" % hparams.n_ctx)
+
+    with tf.Session(graph=tf.Graph()) as sess:
+        np.random.seed(seed)
+        tf.set_random_seed(seed)
+
+        output = sample.sample_sequence(
+            hparams=hparams, length=length,
+            start_token=enc.encoder['<|endoftext|>'],
+            batch_size=batch_size,
+            temperature=temperature, top_k=top_k, top_p=top_p
+        )[:, 1:]
+
+        saver = tf.train.Saver()
+        ckpt = tf.train.latest_checkpoint(os.path.join(models_dir, model_name))
+        saver.restore(sess, ckpt)
+
+        return enc, output, ckpt, hparams
+
+def predict(enc, ckpt, hparams, output, batch_size=1):
+    with tf.Session(graph=tf.Graph()) as sess:
+
+        out = sess.run(output)
+        for i in range(batch_size):
+            text = enc.decode(out[i])
+            return text
