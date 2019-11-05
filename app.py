927e00b4b4f58f89fc637f74631666b0f81d49a2
Galleom <enc.eng@uea.edu.br>
2019-11-05 15:57:11 -0400

    gpt-2 simple

--- app.py
+++ app.py
@@ -1,23 +1,46 @@
 # app.py
 from flask import Flask, request, jsonify
+import tensorflow as tf
 import os
+import gc
 app = Flask(__name__)
 
 import generate
 
-graph = generate.load_graph('.', 'text_model')
+sess = generate.start_tf_sess(threads=1)
+generate.load_gpt2(sess)
 
-@app.route('/')
+# Needed to avoid cross-domain issues
+response_header = {
+    'Access-Control-Allow-Origin': '*'
+}
+generate_count = 0
+
+@app.route('/', methods=['GET'])
 def index():
-    return "<h1>Welcome to our server !!</h1>"
+    global generate_count
+    global sess
+    params = request.query_params
+    text = generate.generate(sess,
+                      length=1024,
+                      model_name="text_model",
+                      include_prefix=False,
+                      return_as_list=True
+                         )[0]
 
-@app.route('/result', methods=['GET'])
-def get_something():
-    return jsonify({
-        "Message": generate.predict(graph),
-        # Add this option to distinct the POST request
-        "METHOD" : "GET"
-    })
+    generate_count += 1
+    if generate_count == 8:
+        # Reload model to prevent Graph/Session from going OOM
+        tf.reset_default_graph()
+        sess.close()
+        sess = generate.start_tf_sess(threads=1)
+        generate.load_gpt2(sess)
+        generate_count = 0
+        
+
+    gc.collect()
+    return jsonify({'text': text},
+                         headers=response_header)
 
 @app.route('/results/', methods=['POST'])
 def post_something():
--- generate.py
+++ generate.py
@@ -1,49 +1,150 @@
-import json
 import os
+import json
+import requests
+import sys
+import shutil
+import re
+from tqdm import tqdm, trange
 import numpy as np
 import tensorflow as tf
-
+from tensorflow.core.protobuf import rewriter_config_pb2
+import time
+import csv
+import argparse
 import model, sample, encoder
 
-def load_graph(models_dir, model_name, temperature=0.8, top_k=40,top_p=1,batch_size=1, length=None, seed = None):
-    models_dir = os.path.expanduser(os.path.expandvars(models_dir))
-    enc = encoder.get_encoder(model_name, models_dir)
+def start_tf_sess(threads=1, server=None):
+    """
+    Returns a tf.Session w/ config
+    """
+    config = tf.compat.v1.ConfigProto()
+    config.gpu_options.allow_growth = True
+    config.graph_options.rewrite_options.layout_optimizer = rewriter_config_pb2.RewriterConfig.OFF
+    if threads > 0:
+        config.intra_op_parallelism_threads = threads
+        config.inter_op_parallelism_threads = threads
+
+    if server is not None:
+        return tf.compat.v1.Session(target=server.target, config=config)
+    
+    return tf.compat.v1.Session(config=config)
+
+
+def load_gpt2(sess,
+              run_name="run1",
+              model_name=None,
+              model_dir='models'):
+    """Loads the model checkpoint or existing model into a TensorFlow session
+    for repeated predictions.
+    """
+
+    checkpoint_path = os.path.join(model_dir, model_name)
+
     hparams = model.default_hparams()
-    with open(os.path.join(models_dir, model_name, 'hparams.json')) as f:
+    with open(os.path.join(checkpoint_path, 'hparams.json')) as f:
         hparams.override_from_dict(json.load(f))
 
-    if length is None:
-        length = hparams.n_ctx
-    elif length > hparams.n_ctx:
-        raise ValueError("Can't get samples longer than window size: %s" % hparams.n_ctx)
+    context = tf.compat.v1.placeholder(tf.int32, [1, None])
+    output = model.model(hparams=hparams, X=context)
 
-    with tf.Session(graph=tf.Graph()) as sess:
-        np.random.seed(seed)
-        tf.set_random_seed(seed)
+    ckpt = tf.train.latest_checkpoint(checkpoint_path)
+    saver = tf.compat.v1.train.Saver(allow_empty=True)
+    sess.run(tf.compat.v1.global_variables_initializer())
 
-        output = sample.sample_sequence(
-            hparams=hparams, length=length,
-            start_token=enc.encoder['<|endoftext|>'],
-            batch_size=batch_size,
-            temperature=temperature, top_k=top_k, top_p=top_p
-        )[:, 1:]
+    print('Loading pretrained model', ckpt)
+    saver.restore(sess, ckpt)
 
-        saver = tf.train.Saver()
-        ckpt = tf.train.latest_checkpoint(os.path.join(models_dir, model_name))
-        saver.restore(sess, ckpt)
-        graph = (enc, output, ckpt, hparams, batch_size)
-        print (1)
-        return graph
+def generate(sess,
+             run_name='run1',
+             model_name=None,
+             model_dir='models',
+             sample_dir='samples',
+             return_as_list=False,
+             truncate=None,
+             destination_path=None,
+             sample_delim='=' * 20 + '\n',
+             prefix=None,
+             seed=None,
+             nsamples=1,
+             batch_size=1,
+             length=1023,
+             temperature=0.8,
+             top_k=40,
+             top_p=1.0,
+             include_prefix=True):
+    """Generates text from a model loaded into memory.
+    Adapted from https://github.com/openai/gpt-2/blob/master/src/interactive_conditional_samples.py
+    """
 
-def predict(graph):
-    enc=graph[0]
-    output=graph[1]
-    ckpt=graph[2]
-    hparams=graph[3]
-    batch_size=graph[4]
-    with tf.Session(graph=tf.Graph()) as sess:
+    if batch_size is None:
+        batch_size = 1
+    assert nsamples % batch_size == 0
 
-        out = sess.run(output)
+    if nsamples == 1:
+        sample_delim = ''
+
+    if prefix == '':
+        prefix = None
+    checkpoint_path = os.path.join(model_dir, model_name)
+
+    enc = encoder.get_encoder(model_name)
+    hparams = model.default_hparams()
+    with open(os.path.join(checkpoint_path, 'hparams.json')) as f:
+        hparams.override_from_dict(json.load(f))
+
+    if prefix:
+        context = tf.compat.v1.placeholder(tf.int32, [batch_size, None])
+        context_tokens = enc.encode(prefix)
+
+    np.random.seed(seed)
+    tf.compat.v1.set_random_seed(seed)
+
+    output = sample.sample_sequence(
+        hparams=hparams,
+        length=min(length, 1023 - (len(context_tokens) if prefix else 0)),
+        start_token=enc.encoder['<|endoftext|>'] if not prefix else None,
+        context=context if prefix else None,
+        batch_size=batch_size,
+        temperature=temperature, top_k=top_k, top_p=top_p
+    )[:, 1:]
+
+    if destination_path:
+        f = open(destination_path, 'w')
+    generated = 0
+    gen_texts = []
+    while generated < nsamples:
+        if not prefix:
+            out = sess.run(output)
+        else:
+            out = sess.run(output, feed_dict={
+                    context: batch_size * [context_tokens]
+                })
         for i in range(batch_size):
-            text = enc.decode(out[i])
-            return text
+            generated += 1
+            gen_text = enc.decode(out[i])
+            if prefix:
+                gen_text = enc.decode(context_tokens[:1]) + gen_text
+            if truncate:
+                truncate_esc = re.escape(truncate)
+                if prefix and not include_prefix:
+                    prefix_esc = re.escape(prefix)
+                    pattern = '(?:{})(.*?)(?:{})'.format(prefix_esc,
+                                                         truncate_esc)
+                else:
+                    pattern = '(.*?)(?:{})'.format(truncate_esc)
+
+                trunc_text = re.search(pattern, gen_text, re.S)
+                if trunc_text:
+                    gen_text = trunc_text.group(1)
+            gen_text = gen_text.lstrip('\n')
+            if destination_path:
+                f.write("{}\n{}".format(gen_text, sample_delim))
+            if not return_as_list and not destination_path:
+                print("{}\n{}".format(gen_text, sample_delim), end='')
+            gen_texts.append(gen_text)
+
+    if destination_path:
+        f.close()
+
+    if return_as_list:
+        return gen_texts
