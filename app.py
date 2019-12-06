c24f93f8bdb8b60fef7d3a81c34b2f74c5a42d5c
Chen <Mr.galleom@hotmail.com>
2019-12-06 17:43:29 -0400

    deleted gpt-2-simple and useless stuff

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 # app.py
 from starlette.applications import Starlette
 from starlette.responses import UJSONResponse
-import gpt_2_simple as gpt2
-import uvicorn
-
-
 import tensorflow as tf
+import uvicorn
 import os
 import gc
 
@@ -23,7 +20,7 @@ response_header = {
 generate_count = 0
 
 @app.route('/', methods=['GET'])
-def index():
+async def homepage(request):
     global generate_count
     global sess
     
@@ -49,46 +46,5 @@ def index():
     return jsonify({'text': text},
                          headers=response_header)
 
-@app.route('/results/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {param} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
-
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-# A welcome message to test our server
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
-
-    # For debugging
-    print(f"got name {name}")
-
-    response = {}
-
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
-
-    # Return the response in json format
-    return jsonify(response)
-
 if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
     uvicorn.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
