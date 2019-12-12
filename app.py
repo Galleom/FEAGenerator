ff8ef66d436f7fe9fd62c2a8faef5027e5a5a79f
Chen <Mr.galleom@hotmail.com>
2019-12-12 16:10:56 -0400

    response test

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # app.py
-from flask import Flask
+from flask import Flask, jsonify
 import tensorflow as tf
 import os
 import gc
@@ -38,7 +38,8 @@ async def homepage():
         generate_count = 0
         
     gc.collect()
-    return text
-
+    #return text
+    data = {'name': 'davidism'}
+    return jsonify(data)
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
