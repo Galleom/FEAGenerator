1b344ecce7b78ec255422cb7696449b1893bc787
Chen <Mr.galleom@hotmail.com>
2019-12-12 17:27:14 -0400

    test

--- app.py
+++ app.py
@@ -20,26 +20,7 @@ generate_count = 0
 
 @app.route('/')
 async def homepage():
-    global generate_count
-    global sess
-    text = generate.generate(sess,
-                      length=1024,
-                      model_name="text_model",
-                      include_prefix=False,
-                      return_as_list=True
-                         )[0]
-    generate_count += 1
-    if generate_count == 8:
-        # Reload model to prevent Graph/Session from going OOM
-        tf.reset_default_graph()
-        sess.close()
-        sess = generate.start_tf_sess(threads=1)
-        generate.load_gpt2(sess)
-        generate_count = 0
-        
-    gc.collect()
-    #return text
-    data = {'name': 'davidism'}
-    return jsonify(data)
+    target = os.environ.get('TARGET', 'World')
+    return 'Hello {}!\n'.format(target)
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
