b96f65249765b255fe2c04622fc44edfc1b87bf9
Chen <Mr.galleom@hotmail.com>
2019-12-12 17:37:50 -0400

    test 2

--- app.py
+++ app.py
@@ -9,18 +9,10 @@ app = Flask(__name__)
 
 import generate
 
-sess = generate.start_tf_sess(threads=1)
-generate.load_gpt2(sess, model_name="text_model")
-
-# Needed to avoid cross-domain issues
-response_header = {
-    'Access-Control-Allow-Origin': '*'
-}
-generate_count = 0
-
 @app.route('/')
 async def homepage():
     target = os.environ.get('TARGET', 'World')
     return 'Hello {}!\n'.format(target)
+
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
