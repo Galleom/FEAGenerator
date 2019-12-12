91c14a02b5d3c7e0079efe643e7745cdc0f7fac3
Chen <Mr.galleom@hotmail.com>
2019-12-12 18:22:14 -0400

    test 3

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 # app.py
 from flask import Flask, jsonify
-import tensorflow as tf
+#import tensorflow as tf
 import os
-import gc
+#import gc
+#import generate
 
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-import generate
 
-@app.route('/')
+@application.route('/')
 async def homepage():
     target = os.environ.get('TARGET', 'World')
     return 'Hello {}!\n'.format(target)
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+    application.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
